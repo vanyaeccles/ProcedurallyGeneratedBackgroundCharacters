a7a8823bc9e989cfe402c520d5fb088aea3dcd0c
ITSERV\ecclesva <ecclesva@tcd.ie>
2017-05-31 18:30:13 +0100

    backbone of utility agent

---
+++ WDPDAD/WhatDoPeopleDoAllDay/Assets/Scripts/ActionBehaviour.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionBehaviour : MonoBehaviour
+{
+
+    public string actionName;
+    public float utilityValueTotal;
+
+
+    public bool isEvaluating;
+
+    
+    public List<ActionConsiderations> actionConsiderations;
+
+
+
+
+
+    #region Utility Evaluation
+
+    public float EvaluateUtility(Agent agent)
+    {
+        EvaluateActionConsiderations(agent);
+
+        return utilityValueTotal;
+    }
+
+    //Evaluate overall utility for the action
+    public void EvaluateActionConsiderations(Agent agent)
+    {
+        resetUtilityTotal();
+
+        int numbEvaluatedConsiderations = 0;
+        
+        for(int i = 0; i < actionConsiderations.Count; i++)
+        {
+            if(actionConsiderations[i].enabled)
+            {
+                utilityValueTotal += actionConsiderations[i].utilityValue * actionConsiderations[i].considerationWeight;
+                numbEvaluatedConsiderations++;
+            }
+        }
+
+        utilityValueTotal /= numbEvaluatedConsiderations;
+    }
+
+    private void resetUtilityTotal()
+    {
+        utilityValueTotal = 0.0f;
+    }
+
+    #endregion
+
+
+
+
+
+
+
+    #region Behaviour
+
+    #endregion
+}
---
+++ WDPDAD/WhatDoPeopleDoAllDay/Assets/Scripts/ActionConsiderations.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionConsiderations : MonoBehaviour
+{
+
+    public float utilityValue; // value returned for action utility evaluation
+    public float considerationWeight; // value that weighs in on the agent's personal evaluation of this consideration, maybe stored in the agent instead? @TODO
+    
+
+
+    public float calculateUtility(int funcType, float normalizedInput)
+    {
+        // use the function type to specify the function, feed in the normalized input value and get the result
+
+        return utilityValue;
+    }
+	
+
+
+    // Possibly don't need this @TODO
+    public float normalizeUtility(float util)
+    { 
+        return util;
+    }
+}
