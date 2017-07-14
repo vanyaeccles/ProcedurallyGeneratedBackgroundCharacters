a25a1fe9accfc3dc030795105c9487a3761875d9
ITSERV\ecclesva <ecclesva@tcd.ie>
2017-07-14 19:58:52 +0100

    Added time of day variable + simple sun

---
+++ WDPDAD/WhatDoPeopleDoAllDay/Assets/Scripts/Global/DayNightCycle.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNightCycle : MonoBehaviour {
+
+    public AgentStateVarFloat time;
+
+    float timeOfDay = 12.0f;
+     
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //update time
+        timeOfDay += 0.3f * UtilityTime.time;
+
+        if (timeOfDay >= 24.0f)
+            timeOfDay = 0.0f;
+
+
+
+        time.value = timeOfDay;
+    }
+}
---
+++ WDPDAD/WhatDoPeopleDoAllDay/Assets/Scripts/Global/SunScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunScript : MonoBehaviour {
+
+    public AgentStateVarFloat time;
+
+    public float angle = 0.0f;
+
+	// Update is called once per frame
+	void Update ()
+    {
+
+        //float timeValue = time.value / 24.0f;
+
+        angle = 5.0f * UtilityTime.time;
+
+        // @TODO fix this
+        transform.RotateAround(Vector3.zero, Vector3.forward,  angle);
+
+
+    }
+}
