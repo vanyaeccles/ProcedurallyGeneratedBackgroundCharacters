9929fc30767040a775c7cc5fdbb14c562f579ef1
ITSERV\ecclesva <ecclesva@tcd.ie>
2017-07-14 20:16:32 +0100

    Sun now moves at correct angle per frame

--- WDPDAD/WhatDoPeopleDoAllDay/Assets/Scripts/Global/SunScript.cs
+++ WDPDAD/WhatDoPeopleDoAllDay/Assets/Scripts/Global/SunScript.cs
@@ -8,13 +8,21 @@ public class SunScript : MonoBehaviour {
 
     public float angle = 0.0f;
 
-	// Update is called once per frame
-	void Update ()
+    float hoursPerFrame = 0.0f;
+
+    float anglesPerHour = 15.0f;
+
+    // Update is called once per frame
+    void Update ()
     {
 
         //float timeValue = time.value / 24.0f;
 
-        angle = 5.0f * UtilityTime.time;
+        //angle = 5.0f * UtilityTime.time;
+
+        hoursPerFrame = (0.3f * UtilityTime.time);
+
+        angle = hoursPerFrame * anglesPerHour;
 
         // @TODO fix this
         transform.RotateAround(Vector3.zero, Vector3.forward,  angle);
